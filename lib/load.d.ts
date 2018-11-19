a2e6526c4a59e225ed14d5328a7e899302edef93
Martin Neundorfer <m.neundorfer@labor.digital>
2018-11-19 11:59:28 +0100

    - Adds missing load() lib files

---
+++ lib/load.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Martin Neundorfer on 16.11.2018.
+ * For LABOR.digital
+ */
+import { LoadOptions } from "./load.interfaces";
+export declare function load(src: string, options?: LoadOptions): Promise<string>;
---
+++ lib/load.interfaces.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Martin Neundorfer on 16.11.2018.
+ * For LABOR.digital
+ */
+export interface LoadOptions {
+    /**
+     * The number of milliseconds before a chunk request should be marked as "timedout"
+     */
+    timeout?: number;
+}
