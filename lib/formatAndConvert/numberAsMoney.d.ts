b9f176c40813790357791f7f5e2f284b18bd787c
Martin Neundorfer <m.neundorfer@labor.digital>
2018-12-07 21:25:22 +0100

    - Adds an additional "thousandSeparator" flag to numberAsMoney which is used to drop the periods as separators between number groups

--- lib/formatAndConvert/numberAsMoney.d.ts
+++ lib/formatAndConvert/numberAsMoney.d.ts
@@ -7,5 +7,6 @@
  * The output format will be 1.000.000,00 or 1.000.000 if "dropDecimals" is set to true
  * @param value The value to convert into a number format
  * @param dropDecimals True to drop the decimal numbers in the output
+ * @param thousandSeparator Set to FALSE to disable the thousand separators
  */
-export declare function numberAsMoney(value: number, dropDecimals?: boolean): string;
+export declare function numberAsMoney(value: number, dropDecimals?: boolean, thousandSeparator?: boolean): string;
--- src/formatAndConvert/numberAsMoney.ts
+++ src/formatAndConvert/numberAsMoney.ts
@@ -7,14 +7,20 @@
  * The output format will be 1.000.000,00 or 1.000.000 if "dropDecimals" is set to true
  * @param value The value to convert into a number format
  * @param dropDecimals True to drop the decimal numbers in the output
+ * @param thousandSeparator Set to FALSE to disable the thousand separators
  */
-export function numberAsMoney(value: number, dropDecimals?: boolean): string {
-	if(typeof value === "string") value = parseFloat(value);
+export function numberAsMoney(value: number, dropDecimals?: boolean, thousandSeparator?: boolean): string {
+	if (typeof value === "string") value = parseFloat(value);
 	if (typeof value !== "number" || isNaN(value)) return "ERR";
 	// Make sure every browser has the correct base format
 	let result = (Math.round(value * 100) / 100).toFixed(2);
 
-	if (dropDecimals)
-		return result.replace(/\..*?$/g, "").replace(/(\d)(?=(\d{3})+$)/g, "$1.");
-	return result.replace(/\./g, ",").replace(/(\d)(?=(\d{3})+,)/g, "$1.");
+	if (dropDecimals) {
+		result = result.replace(/\..*?$/g, "");
+		if (thousandSeparator !== false) return result.replace(/(\d)(?=(\d{3})+$)/g, "$1.");
+	} else {
+		result = result.replace(/\./g, ",");
+		if (thousandSeparator !== false) return result.replace(/(\d)(?=(\d{3})+,)/g, "$1.");
+	}
+	return result;
 }
