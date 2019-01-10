0f0f89cb0b645b2ed511188fabd17a8313051d0a
Martin Neundorfer <m.neundorfer@labor.digital>
2019-01-10 15:15:11 +0100

    - test

---
+++ src/FormatAndConvert/asArray2.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by Martin Neundorfer on 24.09.2018.
+ * For LABOR.digital
+ */
+import {forEach} from "../Lists/forEach";
+import {List} from "../Interfaces/List";
+import {getListType, ListType} from "../Lists/listAccess";
+import {isFunction} from "../Types/isFunction";
+
+/**
+ * Converts a given object/map/set into an array
+ * @param list
+ * @return {Array}
+ */
+export function asArray(list: List): Array<any> {
+	if (list === null || typeof list === "undefined") return [];
+	const outputType = getListType(list);
+	if (outputType === ListType.NoList) throw new Error("Could not determine the output type of a given element!");
+	if (isFunction(Array.from)) return Array.from(list as any);
+	const output = [];
+	forEach(list, v => output.push(v));
+	return output;
+}
