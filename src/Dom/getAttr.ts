2aec48cc6e5ad34890b0554c52026ef50ec69702
Martin Neundorfer <m.neundorfer@labor.digital>
2019-05-17 14:06:22 +0200

    feat: add helpers to manipulate dom element attributes

---
+++ src/Dom/getAttr.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 LABOR.digital
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified: 2019.05.07 at 18:19
+ */
+
+import {isObject} from "../Types/isObject";
+
+/**
+ * Returns the value of one attribute of the given element
+ * @param element the element to read the attribute on
+ * @param attributeName the name of the attribute to look up (e.g. class)
+ * @param fallback Optional fallback to be returned when the attribute was not found
+ */
+export function getAttr(element: HTMLElement | Element, attributeName: string, fallback?: any) {
+	if (!isObject(element)) return undefined;
+	if (!element.hasAttribute(attributeName)) return fallback;
+	return element.getAttribute(attributeName);
+}
---
+++ src/Dom/setAttr.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 LABOR.digital
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified: 2019.05.07 at 18:22
+ */
+
+import {forEach} from "../Lists/forEach";
+import {isBool} from "../Types/isBool";
+import {isNumber} from "../Types/isNumber";
+import {isString} from "../Types/isString";
+import {isUndefined} from "../Types/isUndefined";
+
+/**
+ * Internal helper
+ *
+ * @param element
+ * @param attributeName
+ * @param value
+ */
+function setAttrSetter(element: HTMLElement | Element, attributeName: string, value: string | null) {
+	if (value === null) element.removeAttribute(attributeName);
+	else element.setAttribute(attributeName, value);
+}
+
+/**
+ * Sets / removes a given attribute from the list of given elements
+ * @param element the element / elements to set or remove the attribute for
+ * @param attributeName The attribute to set / remove
+ * @param value If null is given the attribute will be removed, otherwise the attribute will be set to this value
+ */
+export function setAttr(element: HTMLElement | NodeListOf<Element>, attributeName: string, value: string | number | boolean | null) {
+	let valueString = null;
+	if (isString(value)) valueString = value as string;
+	else if (isBool(value)) valueString = value ? "TRUE" : "FALSE";
+	else if (isNumber(value)) valueString = value + "";
+	if (isUndefined((element as NodeListOf<Element>).length))
+		setAttrSetter(element as HTMLElement, attributeName, valueString);
+	forEach(element, (e) => setAttrSetter(e, attributeName, valueString));
+}
