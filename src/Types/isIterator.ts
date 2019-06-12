8ada1f2b07fcdba6bf668da956f4cf4bcd2b6cee
Martin Neundorfer <m.neundorfer@labor.digital>
2019-06-12 22:42:43 +0200

    feat: add isIterator helper

---
+++ src/Types/isIterator.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 LABOR.digital
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified: 2019.06.12 at 12:43
+ */
+
+/**
+ * Checks if a given value is an iterator or not
+ * @param value
+ */
+export function isIterator(value): boolean {
+	return typeof value === "object" && value != null && typeof value[Symbol.iterator] === "function";
+}
