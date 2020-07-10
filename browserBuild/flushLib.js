37a7724b3ccce2e5b6ab377ea10d56cbf77a4362
Martin Neundorfer <m.neundorfer@labor.digital>
2020-07-10 12:38:40 +0200

    fix: fix browser build

--- browserBuild/flushLib.js
+++ browserBuild/flushLib.js
@@ -15,25 +15,28 @@
  *
  * Last modified: 2020.01.02 at 16:25
  */
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-function rmdirRecursive(dirname, removeSelf) {
-	if (fs.existsSync(dirname)) {
-		fs.readdirSync(dirname).forEach(function (file, index) {
-			var curPath = dirname + path.sep + file;
-			if (fs.lstatSync(curPath).isDirectory()) { // recurse
-				rmdirRecursive(curPath);
-			} else { // delete file
-				fs.unlinkSync(curPath);
-			}
-		});
-		if (removeSelf !== false) fs.rmdirSync(dirname);
-	}
+function rmdirRecursive(dirname, removeSelf)
+{
+    if (fs.existsSync(dirname)) {
+        fs.readdirSync(dirname).forEach(function (file, index) {
+            var curPath = dirname + path.sep + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                rmdirRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        if (removeSelf !== false) {
+            fs.rmdirSync(dirname);
+        }
+    }
 }
 
 try {
-	rmdirRecursive(__dirname + "/../lib.browser", false);
+    rmdirRecursive(__dirname + '/../lib.browser', false);
 } catch (e) {
-	console.error("Failed to clear lib!");
+    console.error('Failed to clear lib!');
 }
--- browserBuild/webpack.config.js
+++ browserBuild/webpack.config.js
@@ -16,38 +16,43 @@
  * Last modified: 2020.01.02 at 16:25
  */
 
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 // Find all functions we have in our library...
-const dir = "../lib/";
+const dir = '../lib/';
 const entryPoints = {};
 const traverser = function (pathString) {
-	fs.readdirSync(pathString).forEach(fileName => {
-		const localPathString = path.join(pathString, fileName);
-		const fStats = fs.statSync(localPathString);
-		if (fStats.isDirectory()) {
-			traverser(localPathString);
-		} else {
-			if (localPathString.match(/\.([^.]+)$/, "")[1] === "js") {
-				const baseName = path.basename(localPathString).replace(/\.([^.]+)$/, "");
-				entryPoints[baseName] = localPathString;
-			}
-		}
-	});
+    fs.readdirSync(pathString).forEach(fileName => {
+        const localPathString = path.join(pathString, fileName);
+        const fStats = fs.statSync(localPathString);
+        if (fStats.isDirectory()) {
+            traverser(localPathString);
+        } else {
+            // Ignore node modules
+            if (localPathString.match(/[\\\/]Node[\\\/]/)) {
+                return;
+            }
+            
+            if (localPathString.match(/\.([^.]+)$/, '')[1] === 'js') {
+                const baseName = path.basename(localPathString).replace(/\.([^.]+)$/, '');
+                entryPoints[baseName] = localPathString;
+            }
+        }
+    });
 };
 traverser(dir);
 
 module.exports = {
-	mode: "production",
-	entry: entryPoints,
-	output: {
-		path: path.join(__dirname, "../lib.browser"),
-		filename: "Helferlein.[name].js",
-		library: ["Helferlein"],
-		libraryTarget: "umd"
-	},
-	performance: {
-		hints: false
-	}
+    mode: 'production',
+    entry: entryPoints,
+    output: {
+        path: path.join(__dirname, '../lib.browser'),
+        filename: 'Helferlein.[name].js',
+        library: ['Helferlein'],
+        libraryTarget: 'umd'
+    },
+    performance: {
+        hints: false
+    }
 };
