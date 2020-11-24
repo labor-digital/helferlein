f0a9a1af9c55f68804e538f45cbc7c523e1003fd
Martin Neundorfer <m.neundorfer@labor.digital>
2020-11-24 04:41:10 +0100

    feat: build cjs and esm in separate files + deprecate lib directory

--- flushLib.js
+++ flushLib.js
@@ -15,25 +15,29 @@
  *
  * Last modified: 2019.01.30 at 13:21
  */
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-function rmdirRecursive(dirname, removeSelf) {
-	if (fs.existsSync(dirname)) {
-		fs.readdirSync(dirname).forEach(function (file, index) {
-			var curPath = dirname + path.sep + file;
-			if (fs.lstatSync(curPath).isDirectory()) { // recurse
-				rmdirRecursive(curPath);
-			} else { // delete file
-				fs.unlinkSync(curPath);
-			}
-		});
-		if (removeSelf !== false) fs.rmdirSync(dirname);
-	}
+function rmdirRecursive(dirname, removeSelf)
+{
+    if (fs.existsSync(dirname)) {
+        fs.readdirSync(dirname).forEach(function (file, index) {
+            var curPath = dirname + path.sep + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                rmdirRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        if (removeSelf !== false) {
+            fs.rmdirSync(dirname);
+        }
+    }
 }
 
 try {
-	rmdirRecursive(__dirname + "/lib", false);
+    rmdirRecursive(__dirname + '/lib', false);
+    rmdirRecursive(__dirname + '/dist', false);
 } catch (e) {
-	console.error("Failed to clear lib!");
+    console.error("Failed to clear lib or dist!");
 }
