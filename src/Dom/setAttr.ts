50aec8cfd2be7c6067fc79183ffdda9b201a2401
Martin Neundorfer <m.neundorfer@labor.digital>
2021-04-14 11:12:09 +0200

    feat(setAttr): allow to pass attribute object
    Also fix an issue that occurred when an HTMLElement was passed, that had a "length" property. For example select boxes

--- src/Dom/setAttr.ts
+++ src/Dom/setAttr.ts
@@ -16,28 +16,91 @@
  * Last modified: 2019.05.07 at 18:22
  */
 
+import type {PlainObject} from '../Interfaces/PlainObject';
 import {forEach} from '../Lists/forEach';
 import {isBool} from '../Types/isBool';
 import {isNumber} from '../Types/isNumber';
-import {isObject} from '../Types/isObject';
+import {isPlainObject} from '../Types/isPlainObject';
 import {isString} from '../Types/isString';
 import {isUndefined} from '../Types/isUndefined';
 
 /**
- * Internal helper
- *
+ * Routes either a single element or a node list of elements to the attrWriter
  * @param element
  * @param attributeName
  * @param value
  */
-function setAttrSetter(element: HTMLElement | Element, attributeName: string, value: string | null)
+function attrSetSingle(
+    element: HTMLElement | Element | NodeListOf<Element>,
+    attributeName: string,
+    value: string | number | boolean | null
+): void
 {
+    if (element instanceof Element) {
+        attrWriter(element as HTMLElement, attributeName, value);
+    } else {
+        forEach(element, (e) => attrWriter(e, attributeName, value));
+    }
+}
+
+/**
+ * Handles the adding and removal of a single attribute on a given element
+ * @param element
+ * @param attributeName
+ * @param value
+ */
+function attrWriter(
+    element: HTMLElement | Element,
+    attributeName: string,
+    value: string | number | boolean | null
+): void
+{
+    if (!attributeName || !(element instanceof HTMLElement)) {
+        return;
+    }
+    
     if (value === null) {
         element.removeAttribute(attributeName);
     } else {
-        element.setAttribute(attributeName, value);
+        element.setAttribute(attributeName, attrSerializer(value));
     }
 }
+
+/**
+ * Helper to convert a given value into a string
+ * @param value
+ */
+function attrSerializer(value: string | number | boolean | null): string
+{
+    if (isString(value)) {
+        return value as string;
+    }
+    
+    if (isBool(value)) {
+        return value ? 'true' : 'false';
+    }
+    
+    if (isNumber(value)) {
+        return value + '';
+    }
+    
+    if (value !== null && !isUndefined(value)) {
+        return JSON.stringify(value);
+    }
+    
+    return value + '';
+}
+
+
+/**
+ * Sets / removes a given attribute from the list of given elements
+ * @param element the element / elements to set or remove the attribute for
+ * @param attributes An object where key is the name of the attribute and value is the value to set
+ */
+export function setAttr(
+    element: HTMLElement | Element | NodeListOf<Element>,
+    attributes: PlainObject<string | number | boolean | null>
+): void
 
 /**
  * Sets / removes a given attribute from the list of given elements
@@ -49,27 +112,27 @@ export function setAttr(
     element: HTMLElement | Element | NodeListOf<Element>,
     attributeName: string,
     value: string | number | boolean | null
-)
+): void
+
+/**
+ * Sets / removes a given attribute from the list of given elements
+ * @param element the element / elements to set or remove the attribute for
+ * @param a
+ * @param b
+ */
+export function setAttr(
+    element: HTMLElement | Element | NodeListOf<Element>,
+    a: string | PlainObject<string | number | boolean | null>,
+    b?: string | number | boolean | null
+): void
 {
-    if (!isObject(element)) {
+    if (!element) {
         return;
     }
     
-    let valueString: string = '';
-    
-    if (isString(value)) {
-        valueString = value as string;
-    } else if (isBool(value)) {
-        valueString = value ? 'TRUE' : 'FALSE';
-    } else if (isNumber(value)) {
-        valueString = value + '';
-    } else if (value !== null && !isUndefined(value)) {
-        valueString = JSON.stringify(value);
-    }
-    
-    if (isUndefined((element as NodeListOf<Element>).length)) {
-        setAttrSetter(element as HTMLElement, attributeName, valueString);
+    if (isPlainObject(a)) {
+        forEach(a, (v, k) => attrSetSingle(element, k, v));
     } else {
-        forEach(element, (e) => setAttrSetter(e, attributeName, valueString));
+        attrSetSingle(element, a, b ?? null);
     }
 }
